c1824849630b8bebd808da9c85947b9a61a27c57
Kanishka Utagikar <kanishka.utagikar@gmail.com>
2025-01-08 22:24:21 +0530

    Update model.py

--- model.py
+++ model.py
@@ -46,3 +46,20 @@ class PositionalEncoding(nn.Module):
         # enabling efficient operations without explicitly reshaping the tensors.
 
         return self.dropout(x)
+    
+class LayerNormalization(nn.Module):
+
+    def __init__(self, features: int, eps:float=10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(features)) # alpha is a learnable parameter
+        self.bias = nn.Parameter(torch.zeros(features)) # bias is a learnable parameter
+
+    def forward(self, x):
+        # x: (batch, seq_len, hidden_size)
+         # Keep the dimension for broadcasting
+        mean = x.mean(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # Keep the dimension for broadcasting
+        std = x.std(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # eps is to prevent dividing by zero or when std is very small
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
